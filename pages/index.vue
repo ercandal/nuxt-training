a67b596797553ba38dc15b701bc90caf9cc5b301
ercandal <ercandall@hotmail.com>
2024-08-13 02:50:09 +0300

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+
+        <NuxtLink to="/about" class="btn btn-primary">About</NuxtLink>
+
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped></style>
